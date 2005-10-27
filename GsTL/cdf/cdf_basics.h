25d397ac230f2fbc9ce79cb5390c8f9e9259a1b5
aboucher <aboucher>
2005-10-27 05:42:04 +0000

    Add a mean and variance functions to Non_parametric_cdf class

--- GsTL/cdf/cdf_basics.h
+++ GsTL/cdf/cdf_basics.h
@@ -17,6 +17,7 @@ class Cdf {
   typedef T value_type;
   virtual value_type inverse(double p) const = 0;
   virtual double prob(value_type z) const = 0;
+
 };
 
 
@@ -55,14 +56,47 @@ class Non_parametric_cdf : public Cdf<T> {
   inline const_p_iterator p_begin() const {return p_values_.begin();}
   inline const_p_iterator p_end() const {return p_values_.end();}
 
+  value_type mean( void )
+  {
+	  double inc_p = 0.005;
+	  double mean = 0.;
+
+	  for(double pp = 0.02; pp<0.98; pp+=inc_p )
+		  mean += inverse( pp )*inc_p;
+	  return  mean;
+  }
+  value_type variance( void ) const
+  {
+	  double inc_p = 0.005;
+	  double s1 = 0.;
+	  double s2 = 0.;
+	  double inv_p;
+
+	  for(double pp = 0.02; pp<0.98; pp+=inc_p ){
+		  inv_p = inverse( pp );
+		  s1 += inv_p*inc_p;
+		  s2 += inv_p*inv_p*inc_p;
+	  }
+/*
+	std::vector< double > ik_pdf(p_values_.size());
+	std::adjacent_difference(p_values_.begin(),p_values_.end(),ik_pdf.begin() );
+	value_type s1 = std::inner_product(ik_pdf.begin(),ik_pdf.end(), z_values_.begin(), 0.0);
+	value_type s2 = std::inner_product(ik_pdf.begin(),ik_pdf.end(), z_values_.begin(), 0.0, 
+		std::plus<double>(),inner_prod_square);
+*/
+	return s2 - s1*s1;
+  }
+
 
  protected:
   std::vector<value_type>  z_values_;
   std::vector<double>      p_values_;
-
 };
 
 template< class T >
 const double Non_parametric_cdf<T>::NaN = -9e30;
+
+//template< typename value_type >
+inline double inner_prod_square(double p, double z) { 	return p*z*z; }
 
 #endif
